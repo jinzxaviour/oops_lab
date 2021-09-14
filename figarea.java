8f4a580b3c5b5ace747667dd6e0ff904733fad88
Jins xaviour <76903238+jinzxaviour@users.noreply.github.com>
2021-09-14 23:00:47 +0530

    Add files via upload
    graphics of fig

---
+++ figarea.java
@@ -0,0 +1,11 @@
+import graphics.*;
+
+public class figarea{
+    public static void main(String[] args) {
+        fig f = new fig();
+        f.tri();
+        f.rect();
+        f.sqr();
+        f.cir();
+    }
+}
